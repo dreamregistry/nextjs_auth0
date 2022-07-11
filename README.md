50907e34ba704ae9719731e2c5ae3109be9c2d14
Jonatan Sombié <jonatan.sombie@gmail.com>
2022-07-11 13:04:25 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs_auth0
+
+DReAM package for creating auth0 application configured for NextJS.
---
+++ main.tf
@@ -0,0 +1,60 @@
+terraform {
+  backend "s3" {}
+
+  required_providers {
+    random = {
+      source  = "registry.terraform.io/hashicorp/random"
+      version = "3.2.0"
+    }
+
+    auth0 = {
+      source  = "registry.terraform.io/auth0/auth0"
+      version = "0.32.0"
+    }
+  }
+}
+
+provider "random" {}
+provider "auth0" {}
+
+resource "random_pet" "client_name" {}
+
+resource "auth0_client" "client" {
+  name                = random_pet.client_name.id
+  description         = "Application configured for nextauth"
+  app_type            = "regular_web"
+  callbacks           = var.callbacks
+  allowed_logout_urls = var.allowed_logout_urls
+  jwt_configuration {
+    alg = "RS256"
+  }
+}
+
+output "AUTH0_CLIENT_ID" {
+  sensitive = true
+  value     = auth0_client.client.client_id
+}
+
+output "AUTH0_CLIENT_SECRET" {
+  sensitive = true
+  value     = auth0_client.client.client_secret
+}
+
+data "auth0_tenant" "current" {}
+
+output "AUTH0_ISSUER" {
+  sensitive = true
+  value     = "https://${data.auth0_tenant.current.domain}"
+}
+
+output "AUTH0_BASE_URL" {
+  sensitive = true
+  value     = var.app_base_url
+}
+
+resource "random_uuid" "secret" {}
+
+output "AUTH0_SECRET" {
+  sensitive = true
+  value     = random_uuid.secret.result
+}
